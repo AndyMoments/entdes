6321ac21143c4ccf5667b9659ec01e4273c33bae
AndyMoments <58293103+AndyMoments@users.noreply.github.com>
2019-12-05 10:35:38 +0100

    Add files via upload

---
+++ entdes/src/ejercicios/Metodos.java
@@ -0,0 +1,24 @@
+package ejercicios;
+
+public class Metodos {
+
+	public static void main(String[] args) {
+		String var1= new String("Hola");
+		String var2=new String("Mundo");
+		String cadena= var2.concat(var1);
+	}
+	
+	public static void numero () {
+		
+		char a = 84;
+		System.out.println(a);
+	}
+	
+	public static void asci() {
+		char b = 8;
+		double c = (double)b;
+		System.out.println(c);
+	}
+	
+	
+}
